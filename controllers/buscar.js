75894e560223f5371440e12322873af1050dab35
MattoDev <mateofonseca.profesional@gmail.com>
2022-05-22 16:46:41 -0500

    serach routes initial structure

---
+++ controllers/buscar.js
@@ -0,0 +1,9 @@
+const { response } = require("express");
+
+const buscar = (req, res = response) => {
+  res.json({ msg: "...Buscar" });
+};
+
+module.exports = {
+  buscar,
+};
--- models/server.js
+++ models/server.js
@@ -11,6 +11,7 @@ class Server {
       users: "/api/users",
       categorias: "/api/categorias",
       productos: "/api/productos",
+      buscar: "/api/buscar",
     };
 
     //Concetar a base de datos
@@ -39,6 +40,7 @@ class Server {
     this.app.use(this.paths.users, require("../routes/user"));
     this.app.use(this.paths.categorias, require("../routes/categorias"));
     this.app.use(this.paths.productos, require("../routes/productos"));
+    this.app.use(this.paths.buscar, require("../routes/buscar"));
   }
 
   lister() {
