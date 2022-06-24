64a8644cf20d8b00f688afa17df437b048cc3f5c
MattoDev <mateofonseca.profesional@gmail.com>
2022-06-23 23:25:36 -0500

    upload files name

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -6,12 +6,18 @@ const cargarArchivo = async (req, res = response) => {
     res.status(400).send({ msg: "No hay archivos que subir" });
     return;
   }
-  //Imagenes
-  const pathCompleto = await subirArchivo(req.files);
 
-  res.json({
-    nombre: pathCompleto,
-  });
+  try {
+    //Imagenes , txt, md
+    // const pathCompleto = await subirArchivo(req.files, ["txt", "md"], "textos");
+    const pathCompleto = await subirArchivo(req.files, undefined, "imgs");
+
+    res.json({
+      nombre: pathCompleto,
+    });
+  } catch (msg) {
+    res.status(400).json({ msg });
+  }
 };
 
 module.exports = {
--- models/server.js
+++ models/server.js
@@ -41,6 +41,7 @@ class Server {
       fileUpload({
         useTempFiles: true,
         tempFileDir: "/tmp/",
+        createParentPath: true,
       })
     );
   }
